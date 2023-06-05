7be50c069fb2e664eb4a31dbe4c2384b65ad6cdb
Pedro-E-Rodrigues <pedroemanuel_08@hotmail.com>
2023-06-05 15:40:29 -0300

    2° Controle de fluxo For

---
+++ src/main/kotlin/Flow.kt
@@ -0,0 +1,15 @@
+
+
+fun main () {
+//for - loop
+//loop infinito
+
+    val str = "Pedro Emanuel"
+    for (c in str) {
+        //println("$c ")
+    }
+    for (i in 0..100 step 5) {
+        print("$i ")
+    }
+
+}
