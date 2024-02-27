3a16f32bb5231df102ee06129e27cbb50da2c30b
Pedro-E-Rodrigues <pedroemanuel_08@hotmail.com>
2024-02-27 19:27:15 -0300

    2 a 4

--- src/main/kotlin/Exercicio2.kt
+++ src/main/kotlin/Exercicio2.kt
@@ -25,6 +25,21 @@ fun converte(anos: Int) {
 
 }
 
+fun letras(str: String): Int {
+    return str.length
+}
+
+fun cubo(n: Int): Int {
+    return n * n * n
+}
+
+fun milhas(milhas: Float): Float{
+    return milhas * 1.6f
+}
 fun main() {
-    converte(2)
+    //converte(2)
+    letras("asdafasgagsahhaghasgha")
+    cubo(3)
+    milhas(100f)
+
 }
