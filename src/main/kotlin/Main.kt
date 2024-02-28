3a40cb957181fde6895b54b66872bd7d24751a45
Pedro-E-Rodrigues <pedroemanuel_08@hotmail.com>
2024-02-28 17:31:52 -0300

    If and Else

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,26 @@
+fun exc2(cargo: String, experiencia: Int): Float {
+    var bonus = 0f
+    if (cargo == "Gerente") {
+        if (experiencia < 2) {
+            bonus = 2000f
+        } else {
+            bonus = 3000f
+        }
+    } else if (cargo == "Coordenador") {
+        if (experiencia < 1) {
+            bonus = 1500f
+        } else {
+            bonus = 1800f
+        }
+    } else if (cargo == "Engenheiro de software") {
+        bonus = 1000f
+    } else if (cargo == "Estagiario") {
+        bonus = 500f
+    }
+    return bonus
+}
 
-
-fun bonus(cargo: String): Float {
+fun exc1(cargo: String): Float {
     var bonus = 0f
     if (cargo == "Gerente") {
         bonus = 2000f
@@ -15,6 +35,8 @@ fun bonus(cargo: String): Float {
 }
 
 fun main() {
-    val bonus = bonus("Gerente")
-    println(bonus)
+    //val bonus = bonus("Gerente", 3)
+    //if (bonus == 0f) {
+      //  println("Cargo Incorreto")
+    //}
 }
