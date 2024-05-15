a46b98922024ad567c2c9c44410526292839109b
Pedro Emanuel <pedroemanuel_07@hotmail.com>
2024-05-15 16:24:27 -0300

    primeira parte do exercicio

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,47 @@
+fun triangulo(){
+    println("informe o lado 1 do triangulo:")
+    val lado1 = readln()
+
+    println("informe o lado 2 do triangulo:")
+    val lado2 = readln()
+
+    println("informe o lado 3 do triangulo:")
+    val lado3 = readln()
+
+    if (lado1 != null && lado1 != "" && lado2 != null && lado2 != "" && lado3 != null && lado3 != "") {
+        val x = lado1.toInt()
+        val y = lado2.toInt()
+        val z = lado3.toInt()
+
+        if (x == y && y == z) {
+            println("É um triangulo equilatero")
+        } else {
+            println("Nao é equilatero")
+        }
+    }
+}
+
+fun quadrado() {
+    println("informe o lado 1:")
+    val lado1 = readln()
+
+    println("informe o lado 2:")
+    val lado2 = readln()
+
+    if (lado1 != null && lado1 != "" && lado2 != null && lado2 != "") {
+        val x = lado1.toInt()
+        val y = lado2.toInt()
+
+        if (lado1 == lado2) {
+            println("é um quadrado")
+        } else {
+            println("nao é um quadrado")
+        }
+    }
+}
+
+
 fun main() {
-    val s = readln()
-    println(s)
+    //quadrado()
+    //triangulo()
 }
