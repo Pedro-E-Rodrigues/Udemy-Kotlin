761f1924eb070570a68059d7fbc8ac7cdc053ac0
Pedro Emanuel <pedroemanuel_07@hotmail.com>
2024-05-27 09:14:18 -0300

    exercising "For"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,43 @@
-fun main() {
-    var i = 0
-    do {
-        println("$i")
-        i++
+fun exc1() {
+    for (i in 1..50) {
+        print("$i ")
+    }
+}
 
-    }while (i <= 10)
+fun exc2() {
+    for (i in 50 downTo 1) {
+        print("$i ")
+    }
+}
+
+fun exc3() {
+    for (i in 1..50) {
+        if (i % 5 != 0){
+            print("$i ")
+        }
+    }
+}
+
+fun exc4() {
+    var soma =0
+    for (i in 1..500) {
+        soma += i
+    }
+    println(soma)
+}
+
+fun exc5(n: Int){
+    for (i in 1..n){
+        for(j in 1..i){
+            print("#")
+        }
+        println()
+    }
+}
+fun main() {
+    //exc1()
+    //exc2()
+    //exc3()
+    //exc4()
+    exc5(10)
 }
