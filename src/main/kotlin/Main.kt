a055baa930ccadd6911f19edc1638ec9e2fdbe6b
Pedro-E-Rodrigues <pedroemanuel_08@hotmail.com>
2024-09-17 07:24:46 -0300

    Arithmetic Exception

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,13 @@
 fun main(){
    try {
        var s : String? = null
-
+       val a = 10/0
        println(s!!.length)
    } catch (e: NullPointerException) {
        println("Variavel nula!")
    }
+    catch (e: ArithmeticException){
+        println("Impossivel dividir por zero")
+    }
     println("Fim")
 }
