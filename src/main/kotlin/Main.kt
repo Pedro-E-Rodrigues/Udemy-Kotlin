bdb7721d3b5df49a23e0fa8d4661bb83663c32e3
Pedro-E-Rodrigues <pedroemanuel_08@hotmail.com>
2024-09-17 07:07:03 -0300

    Null safety

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,5 @@
-fun main() {
-    var salario = 10000f
-    var patAna = 0f
-    var patPaula = 0f
-    var mes = 0
+fun main(){
+    var s : String? = null
 
-    do {
-        patAna += (salario * 0.05f) + (salario * 0.05f) + (patAna * 0.002f)
-        patPaula += (salario * 0.05f) + (patPaula * 0.008f)
-        mes++
-
-        println("pat Ana - $patAna ------- pat Paula - $patPaula")
-
-    }while (patAna>patPaula)
-
-    println("Ana - ${(patAna * 0.002f)}")
-    println("Paula - ${(patPaula* 0.008f)}")
-    println("Paula vai passar o patrimoinio de ana no mes $mes")
+    println(s?.length)
 }
