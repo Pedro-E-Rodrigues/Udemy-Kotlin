2e431cfefa483a988cb677641ab655bc88d73f77
Pedro-E-Rodrigues <pedroemanuel_08@hotmail.com>
2024-09-17 07:32:22 -0300

    Generic Exception

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,16 @@
 fun main(){
    try {
        var s : String? = null
-       val a = 10/0
        println(s!!.length)
+       val a = 10/0
    } catch (e: NullPointerException) {
        println("Variavel nula!")
    }
     catch (e: ArithmeticException){
         println("Impossivel dividir por zero")
+    }
+    catch (e: Exception){
+        println("Generica")
     }
     println("Fim")
 }
