920c4b9545bdc15405a862dd3da599cd9db0d329
Pedro-E-Rodrigues <pedroemanuel_08@hotmail.com>
2024-10-31 13:31:37 -0300

    Generics uses

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,10 @@
 fun main(){
-    println(media(10f, 8f))
+    println(media(false,10f, 8f,"",false,9f))
+
+    arrayOf(0,2,3,6,2,6,45f,false)
 }
 
-fun <T> media(vararg notas: T ): Float{
+fun <T, J> media(abc: J,vararg notas: T ): Float{
     var soma = 0f
     for (n in notas){
         if (n is Float) {
