5e8f02ef890f423f4b4542ca7bac7bc08fcf42ef
Pedro-E-Rodrigues <pedroemanuel_08@hotmail.com>
2024-11-04 19:18:09 -0300

    Any

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,10 @@
-fun main(){
-    println(media(false,10f, 8f,"",false,9f))
+fun main (){
+    // Any, Unit, Nothing
 
-    arrayOf(0,2,3,6,2,6,45f,false)
+    valores(1)
+
 }
 
-fun <T, J> media(abc: J,vararg notas: T ): Float{
-    var soma = 0f
-    for (n in notas){
-        if (n is Float) {
-            soma += n
-        }
-
-    }
-    return (soma / notas.size)
+fun valores(value :Any){
+//(Any) aceita qualquer tipo de valor
 }
