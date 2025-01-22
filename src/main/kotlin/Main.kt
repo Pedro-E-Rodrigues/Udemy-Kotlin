887439885fbf2d8bf4e29ab2bf86d2721d8db054
Pedro-E-Rodrigues <pedroemanuel_08@hotmail.com>
2025-01-22 11:41:02 -0300

    Constructors

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,8 @@
 class Pessoa(val anoNascimento: Int, var nome: String) {
+    var documento: String = ""
+
+    constructor(anoNascimento: Int, nome: String, doc: String) : this(anoNascimento, nome)
+
     fun acordar() {
 
     }
@@ -12,8 +16,9 @@ fun main() {
     // class - comportamentos e atributos
 
     // classe (Instancia) objetos
-    var pessoa: Pessoa = Pessoa(2003, "Pedro")
+    var pessoa: Pessoa = Pessoa(2003, "Pedro", doc = "2156416439")
 
+    //this - se refere ao objeto
     pessoa.nome
 
     pessoa.acordar()
