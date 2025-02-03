91d98a4bd418695ffacb7f5a82521e70f6c8282f
Pedro-E-Rodrigues <pedroemanuel_08@hotmail.com>
2025-02-03 12:21:08 -0300

    equal

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main(){
      val f1: Forma = Forma(10,20)
      val f2: Forma = Forma(10,20)
-    f1.equals(f2)
+    println(f1.equals(f2))
     println(f1.toString())
     println(f1.hashCode())
     println("------------")
@@ -10,5 +10,14 @@ fun main(){
 }
 
 
-class Forma(val a:Int,val b:Int)
+class Forma(val a:Int,val b:Int){
+    //o valor vai dar True devido o código abaixo
+    override fun equals(other: Any?): Boolean {
+        return if (other is Forma) {
+            this.a == other.a && this.b == other.b
+        } else {
+            false
+        }
+    }
+}
 data class FormaData(val a: Int, val b: Int)
