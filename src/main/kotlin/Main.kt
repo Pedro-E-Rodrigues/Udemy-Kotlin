3ad06cff605bac7dc18a17e302d6a12e0ab1e25e
Pedro-E-Rodrigues <pedroemanuel_08@hotmail.com>
2025-02-03 12:38:51 -0300

    conceitos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,25 @@
 fun main(){
+    // objeto (f1) -> endereço mem1
+    // objeto (f2) -> endereço mem2
+
      val f1: FormaData = FormaData(10,20)
-     val f2: FormaData = FormaData(10,20)
+     var f2: FormaData
+
+     f2 = f1.copy(50)
+
     println(f1.equals(f2))
     println(f1.toString())
     println(f1.hashCode())
     println("------------")
     println(f2.toString())
     println(f2.hashCode())
+
+    f2 = f1.copy()
 }
 
 
 class Forma(val a:Int,val b:Int)
 data class FormaData(val a: Int, val b: Int) {}
+
+data class Endereço(val rua:String,val cep: String, val city:String)
+fun endereço(e: Endereço){}
