54aac9a0e4bf546ad18cc71c9edc44c4deb93f08
Pedro-E-Rodrigues <pedroemanuel_08@hotmail.com>
2025-02-04 11:27:55 -0300

    Herança

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,16 @@
-fun main(){
-    // objeto (f1) -> endereço mem1
-    // objeto (f2) -> endereço mem2
-
-     val f1: FormaData = FormaData(10,20)
-     var f2: FormaData
-
-     f2 = f1.copy(50)
-
-    println(f1.equals(f2))
-    println(f1.toString())
-    println(f1.hashCode())
-    println("------------")
-    println(f2.toString())
-    println(f2.hashCode())
-
-    f2 = f1.copy()
+open class Eletronico(var marca: String) {
+    fun ligar() {}
+    fun desligar() {}
 }
 
+class Computador(marca: String) : Eletronico(marca) {
+    fun instalarSoftware() {}
+    fun processar() {}
+}
 
-class Forma(val a:Int,val b:Int)
-data class FormaData(val a: Int, val b: Int) {}
-
-data class Endereço(val rua:String,val cep: String, val city:String)
-fun endereço(e: Endereço){}
+fun main() {
+    var c = Computador("DELL")
+    c.ligar()
+    c.desligar()
+    c.instalarSoftware()
+}
